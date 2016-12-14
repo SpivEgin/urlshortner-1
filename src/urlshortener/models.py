e8d5bb5c2229e2c7bfa5bf467891ae1eabd8fe9d
joseph mathew <jojo.math@gmail.com>
2016-12-13 23:10:25 -0600

    added models

--- src/urlshortener/models.py
+++ src/urlshortener/models.py
@@ -6,11 +6,15 @@ from django.db import models
 
 class KirrURL(models.Model):
 	url = models.CharField(max_length=220, )
+	shortcode  = models.CharField(max_length=20)
+	#shortcode  = models.CharField(max_length=20, default="shortcode")
 
 	def __str__(self):
 		return str(self.url)
 
 	def __unicode__(self):
 		return str(self.url)
+
+	def 
 
 		
